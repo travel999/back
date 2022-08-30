620426d76ebd916040adb0f3d7b5f129d1d1d709
azoong <shshinkitec2@gmail.com>
2022-08-30 20:34:58 +0900

    update

--- routes/posts.js
+++ routes/posts.js
@@ -8,7 +8,7 @@ const postsController = new PostsController();
 router.get('/:postId', postsController.getPost); //일정 조회
 router.post('/',  postsController.createPost) //일정 생성
 router.put("/:_postId",  postsController.updatepost) //일정 수정
-router.delete("/:_postId",  postsController.deletepost) //일정 수정
+router.delete("/:_postId",  postsController.deletepost) //일정 삭제
 
 
 module.exports = router;
