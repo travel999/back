287ddbd3fb478eca83387895a6c39abbe2cee56a
azoong <shshinkitec2@gmail.com>
2022-08-30 21:16:29 +0900

    코멘트삭제

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 const LikesRouter = require("./likes");
 const postsRouter = require("./posts");
-// const Comments = require("./comments");
+
 
 
 
 router.use("/post", postsRouter);
 router.use('/likes', LikesRouter);
-// router.use('/comments/', Comments);
+
 
 module.exports = router;
