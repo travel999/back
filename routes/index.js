a0c520df05fb1e0c733ee9169888a29a886dfa57
azoong <shshinkitec2@gmail.com>
2022-08-30 22:20:06 +0900

    업데이트

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const authMiddleware = require("../middlewares/auth-middleware");
 const router = express.Router();
 
 const UserRouter = require("./users");
 const LikesRouter = require("./likes");
 const postsRouter = require("./posts");
 
-router.use("/users", UserRouter);
-router.use("/post", postsRouter);
-router.use('/likes', LikesRouter);
+router.use("/user", UserRouter);
+router.use("/post", authMiddleware, postsRouter);
+router.use('/likes',authMiddleware, LikesRouter);
 
 
 
