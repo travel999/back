688538236091c2e2a9d86468f17df1e41ce61f77
dongridongil <ehddlfwkd32@naver.com>
2022-09-06 20:45:34 +0900

    [UPDATE]카카오톡소셜로그인구현

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express();
 const logger = require('./logger')
 const Router = require("./routes/index");
 
+// 패스포트연결
+const passport = require("passport");
+const passportConfig = require("./passport/index.js"); // passportIndex
+passportConfig();
+console.log("Passport & kakaoeStrategy _ 설정 완료!");
+
+
 //db연결
 const connect = require("./schemas");
 connect();
@@ -29,11 +36,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/", Router);
 
-//패스포트//////////
-const passport = require("passport");
-const passportConfig = require("./passport/index.js"); // passportIndex
-passportConfig();
-console.log("Passport & kakaoeStrategy _ 설정 완료!");
 
 
 
--- passport/kakao.js
+++ passport/kakao.js
@@ -1,4 +1,3 @@
-
 const passport = require('passport');
 const KakaoStrategy = require('passport-kakao').Strategy;
 const User = require('../schemas/users');
@@ -8,7 +7,7 @@ module.exports = () => {
         new KakaoStrategy(
             {
                 clientID: process.env.KAKAOCLIENT_ID, // 카카오 로그인에서 발급받은 REST API 키
-                callbackURL: "/kakao/callback", // 카카오 로그인 Redirect URI 경로
+                callbackURL: process.env.KAKAOCALLBACKURL, // 카카오 로그인 Redirect URI 경로
             },
 
             // clientID에 카카오 앱 아이디 추가
@@ -21,15 +20,21 @@ module.exports = () => {
                     const exUser = await User.findOne({
                         // 카카오 플랫폼에서 로그인 했고 & snsId필드에 카카오 아이디가 일치할경우
                         userId: profile.id,
+                        provider: "kakao"
                     });
                     // 이미 가입된 카카오 프로필이면 성공
                     if (exUser) {
                         done(null, exUser); // 로그인 인증 완료
                     } else {
+                        // let profile_image = "";
+                        // let email = "";
                         // 가입되지 않는 유저면 회원가입 시키고 로그인을 시킨다
+
                         const newUser = await User.create({
                             userId: profile.id,
-                            profileImage:profile.profile_image,
+                            email: profile._json.kakao_account.email,
+                            nickname: profile.displayName,
+                            profile_image: profile._json.properties.profile_image,
                             provider: 'kakao',
                         });
                         done(null, newUser); // 회원가입하고 로그인 인증 완료
