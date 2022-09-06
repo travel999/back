bf37d044d06096a6865f959f775e08d6c2dc6ea0
azoong <shshinkitec2@gmail.com>
2022-09-07 00:03:24 +0900

    [UPDATE] 일정에 멤버초대 기능추가

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -7,7 +7,6 @@ const User = require("../schemas/users");
 module.exports = (req, res, next) => {
   // const  token  = req.headers.token;
   const {token} = req.cookies;
-  console.log("토큰 확인", token);
 
   if (!token) {
     res.status(401).json({ result: false, error: "로그인이 필요합니다1." });
@@ -19,7 +18,6 @@ module.exports = (req, res, next) => {
     const {userId} = jwt.verify(token, process.env.myKey); // userId 는 jwt.sign(userId : user._id)의 user._id가 할당된다.
     User.findById(userId).then((user) => {
       res.locals.user = user;
-      console.log("유저정보:", res.locals.user);
       next();
     });
   } catch (error) {
