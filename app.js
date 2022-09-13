8c11eff46cbd0c448753e33f56c2831d887f7fd0
hinel03 <Sung.may4th@hotmail.com>
2022-09-13 15:16:36 +0900

    [FIX] 소켓 테스트

--- app.js
+++ app.js
@@ -23,17 +23,17 @@ connect();
 
 app.use(
   cors({
-  // origin: true,
-  origin:[
-  "http://54.180.131.25:3000",
-  "http://localhost:3000",
-  "http://localhost:3000/kakao/callback",
-  "http://localhost:3000/kakao",
-  "http://origachi.s3-website.ap-northeast-2.amazonaws.com",
-  "http://localhost:8000",
-  
-  ],
-  credentials: true
+    // origin: true,
+    origin: [
+      "http://54.180.131.25:3000",
+      "http://localhost:3000",
+      "http://localhost:3000/kakao/callback",
+      "http://localhost:3000/kakao",
+      "http://origachi.s3-website.ap-northeast-2.amazonaws.com",
+      "http://localhost:8000",
+
+    ],
+    credentials: true
   })
 );
 
@@ -62,12 +62,16 @@ app.use((err, req, res, next) => {
 });
 
 
+io.on("connection", (socket) => {
+  console.log("소켓통신 연결");
+
+});
 
 
 
-  server.listen(3000, () => {
-    console.log(3000, '포트로 서버가 열렸어요!');
-  });
+server.listen(3000, () => {
+  console.log(3000, '포트로 서버가 열렸어요!');
+});
 
 
 module.exports = server;
--- socket.js
+++ socket.js
@@ -5,8 +5,4 @@ const server = require("./app");
 const io = socketIo(server);
 const socket = io();
 
-io.on("connection", (socket) => {
-    console.log("소켓통신 연결");
-  
-});
 
