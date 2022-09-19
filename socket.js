77c104212eb5c2a5b08265458baf91161135124e
azoong <shshinkitec2@gmail.com>
2022-09-19 11:21:42 +0900

    [UPDATE] 소켓 기능 app.js => socket.js로 파일분리

--- socket.js
+++ socket.js
@@ -1,11 +1,42 @@
-const http = require("./app");
-const socketIo = require("socket.io");
-const server = require("./app");
+const SocketIo = require("socket.io")
+const Chat = require("./schemas/chat");
 
-const io = socketIo(server);
-const socket = io();
+module.exports = (server, app) => {
+    const io = SocketIo(server, {
+      cors: {
+        origin: '*',
+        credentials: true,
+      },
+    });
+    // app.set('socket.io', io);
 
-io.on("connection", (socket) => {
-    console.log("소켓통신 연결");
-  
-});  
+    io.on("connection", (socket) => {
+        console.log("Connected to Browser ✅")
+        // console.log(`User Connected: ${socket.id}`);
+        
+        socket.on("join_room", (data) => {
+            socket.join(data);
+            console.log(`User with ID: ${socket.id} joined room: ${data}`);
+        });
+        
+        socket.on("send_message", async (messageData) => {
+            log = await Chat.findOne({room : messageData.room})
+            if (log){
+            await Chat.updateOne({ room : messageData.room }, { $push: { chatLog : messageData.message} }) //배열에 메시지 추가
+            socket.to(messageData.room).emit("receive_message", messageData);
+            console.log("메시지보냄1")
+            }else{
+            await Chat.create({room : messageData.room, chatLog : messageData.message })
+            socket.to(messageData.room).emit("receive_message", messageData);
+            console.log("메시지보냄2")
+            }
+            
+            // socket.to(messageData.room).emit("receive_message", messageData);
+        });
+        
+        socket.on("test_send", (data) => {
+            console.log(data.msg);
+            socket.to(data.room).emit("test_receive", data);
+            });
+        })
+    }
