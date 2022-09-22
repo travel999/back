0bfcf9d311bd18175926b0093f287acbd3bde4e6
dongridongil <ehddlfwkd32@naver.com>
2022-09-22 10:14:10 +0900

    [UPDATE]알림삭제기능구현

--- controllers/notis.controller.js
+++ controllers/notis.controller.js
@@ -2,35 +2,33 @@ const NoticeBoard = require('../schemas/noticeboard');
 const NoticeMessage = require('../schemas/noticemessage');
 const Post = require('../schemas/posts');
 
-
+//모든알림메시지확인
 const getAllNotice = async (req, res, next) => {
-
-    const { user } = res.locals;
-    if (user === undefined) {
-        return res.status(401).json({
-            result: 'fail',
-            message: '로그인 후 이용 가능한 기능입니다.',
+    try {
+        const { user } = res.locals;
+        if (user === undefined) {
+            return res.status(401).json({
+                result: 'fail',
+                message: '로그인 후 이용 가능한 기능입니다.',
+            });
+        } console.log(user)
+        const findBoard = await NoticeBoard.findOne({
+            userId: user.nickname,
+        }).populate({
+            path: 'notices'
         });
-
-    } console.log(user)
-    //초대된 nickname2의 noticeboard 안의 NoticeMessage 확인하기
-    const findBoard = await NoticeBoard.findOne({
-        userId: user.nickname,
-    }).populate({
-        path: 'notices'
-    });
-    // await NoticeMessage.where({
-    //     noticeBoardId: findBoard.noticeBoardId,
-    // }).updateMany({ checkNotice: 'true' });
-
-    const result = findBoard.notices;
-    return res.json({
-        result,
-        message: '성공',
-    });
+        const result = findBoard.notices;
+        return res.json({
+            result,
+            message: '성공',
+        });
+    } catch (error) {
+        console.log(err)
+        next(err);
+    }
 };
-//회원가입할때 noticeboard 생성하기
 
+//회원가입할때 noticeboard 생성하기
 const createNoticeBoard = async ({ user }) => {
     try {
         const findExistBoard = await NoticeBoard.findOne({
@@ -47,30 +45,47 @@ const createNoticeBoard = async ({ user }) => {
             return;
         }
     } catch (error) {
-        throw error;
+        console.log(err)
+        next(err);
     }
 };
+
 //초대된 닉네임2 의  noticeboard 안에 noticemessage 넣기
 const createNoticeMessage = async ({ nickname2 }) => {
-
-    const findinviteuser = await Post.findOne({ nickname: nickname2 });
-    const findBoard = await NoticeBoard.findOne({
-        userId: findinviteuser.nickname,
-    });
-    // console.log(findBoard)
-
-    const newMessage = new NoticeMessage({
-        noticeBoardId: findBoard.noticeBoardId,
-        noticeContent: "초대장이 도착했습니다!"
-
-    });
-
-    await newMessage.save();
-    return;
+    try {
+        const findinviteuser = await Post.findOne({ nickname: nickname2 });
+        const findBoard = await NoticeBoard.findOne({
+            userId: findinviteuser.nickname,
+        });
+        const newMessage = new NoticeMessage({
+            noticeBoardId: findBoard.noticeBoardId,
+            noticeContent: "초대장이 도착했습니다!"
+        });
+        await newMessage.save();
+    } catch (error) {
+        console.log(err)
+        next(err);
+    }
 }
-
+//알림삭제하기
+const deleteNotice = async (req, res, next) => {
+    try {
+        const { noticeMessageId } = req.params;
+        const deletenotice = await NoticeMessage.deleteOne({
+            _id: noticeMessageId,
+        });
+        res.json({
+            result: "성공",
+            message: '삭제완료되었습니다'
+        })
+    } catch (error) {
+        console.log(err)
+        next(err);
+    }
+}
 module.exports = {
     getAllNotice,
     createNoticeMessage,
-    createNoticeBoard
+    createNoticeBoard,
+    deleteNotice
 };
