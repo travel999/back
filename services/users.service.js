0a89f24f10094c4e980f2b5d3e2a64917e9b515b
hinel03 <Sung.may4th@hotmail.com>
2022-09-25 16:31:02 +0900

    [FIX] cors 변경

--- services/users.service.js
+++ services/users.service.js
@@ -49,7 +49,7 @@ class UserService {
   sendEmail = async (email) => {
     const regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
     // const code = crypto.randomBytes(10).toString('hex');
-    let code = Math.random().toString().substr(2,8);
+    let code = Math.random().toString(36).substr(2,8);
     if (!regEmail.test(email)) {
       return { result: false, message: "이메일 양식 위반." }
     }
