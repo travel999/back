37c274fcee9fdf2ee198a8b8e3c30d80b41c030a
dongridongil <ehddlfwkd32@naver.com>
2022-09-28 23:43:56 +0900

    [FIX]비밀번호수정에러수정

--- repositories/users.repository.js
+++ repositories/users.repository.js
@@ -109,8 +109,8 @@ class UserRepository {
   };
 
   updatePassword = async (nickname, newPassword) => {
-
-    const updateUser = await User.updateOne({ nickname: nickname }, { $set: { password: newPassword } });
+    const hash = await bcrypt.hash(newPassword, salt);
+    const updateUser = await User.updateOne({ nickname: nickname }, { $set: { password:hash} });
     const userInfo = await User.findOne({ nickname: nickname });
     return userInfo;
   };
