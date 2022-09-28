4fdf511cbe95b980138bdce517778e82790e8dac
azoong <shshinkitec2@gmail.com>
2022-09-28 13:11:40 +0900

    [FIX] multer 이미지처리 보류

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const authMiddlewares = require("../middlewares/auth.middleware");
 const { img_up } = require('../middlewares/user.image');
-
+const User = require("../schemas/users");
 const UserController = require("../controllers/users.controller");
 const usercontroller = new UserController();
 
-// router.post("/signup", multer.uploadProfile.single("img"), usercontroller.createUser);
-router.post("/signup", img_up.single('img'), usercontroller.createUser);
+
+router.post("/signup", usercontroller.createUser);
 router.post("/login", usercontroller.userLogin);
 router.post("/logout", usercontroller.userLogout);
 router.post("/checkEmail", usercontroller.checkEmail);
@@ -16,24 +16,25 @@ router.post("/sendEmail", usercontroller.sendEmail);
 router.post("/checkCode", usercontroller.checkCode);
 
 router.get("/me", authMiddlewares, usercontroller.findUser);
-router.put("/me/image", authMiddlewares, img_up.single('img'), usercontroller.updateImage);
+router.put("/me/image", authMiddlewares, usercontroller.updateImage);
 router.put("/me/password", authMiddlewares, usercontroller.updatePassword);
 router.delete("/me/delete", authMiddlewares, usercontroller.deleteUser);
 router.get('/mine',authMiddlewares,usercontroller.getmine);//나의여행일정 가져오기(닉네임이같아야함)
 
 
-// img_up.single('img'),
+router.put("/me/image", authMiddlewares, img_up.single('img'), usercontroller.updateImage);
+// router.post("/signup", img_up.single('img'), usercontroller.createUser); 회원가입 multer 주석
 
-// router.post("/test", img_up.single('img'),async (req, res, next) => {
+// router.post("/test", img_up.single('img'), async(req, res) => {
 //     // img_up.single('img'),
 //     // console.log (111111, req.file.location)
 //     const uri = req.file.location
 //     console.log(uri)
 //     // const userImage = String(uri)
 //     // const test = "1232satsdat134"
-//     const email = "stasdtst"
-//     // const { email, password} = req.body;
-//     await User.create({ email, uri });
+//     // const email = "stasdtst"
+//     // const { email, nickname } = req.body;
+//     await User.create({ uri });
 //     res.status(200).json({ message: "새로운 유저 정보가 등록되었습니다" })
 
 // })
