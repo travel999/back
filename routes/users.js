f7c8b7f648f415fffd83e0feb5853d7736272dd4
Shin hyun ho <108726282+azoong@users.noreply.github.com>
2022-09-30 21:45:18 +0900

    [TEST] CI 테스트 (#179)
    * [UPDATE] multer S3 이미지 처리 기능
    
    * [TEST] CI 테스트

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const UserController = require("../controllers/users.controller");
 const usercontroller = new UserController();
 
 
-router.post("/signup", usercontroller.createUser);
+router.post("/signup", img_up.single('img'), usercontroller.createUser); // 회원가입 multer 주석
 router.post("/login", usercontroller.userLogin);
 router.post("/logout", usercontroller.userLogout);
 router.post("/checkEmail", usercontroller.checkEmail);
@@ -15,28 +15,16 @@ router.post("/sendEmail", usercontroller.sendEmail);
 router.post("/checkCode", usercontroller.checkCode);
 
 router.get("/me", authMiddlewares, usercontroller.findUser);
-router.put("/me/image", authMiddlewares, usercontroller.updateImage);
+router.put("/me/image", authMiddlewares, img_up.single('img'), usercontroller.updateImage);
 router.put("/me/password", authMiddlewares, usercontroller.updatePassword);
 router.delete("/me/delete", authMiddlewares, usercontroller.deleteUser);
 router.get('/mine',authMiddlewares,usercontroller.getmine);//나의여행일정 가져오기(닉네임이같아야함)
 
 
-// router.put("/me/image", authMiddlewares, img_up.single('img'), usercontroller.updateImage);
-// router.post("/signup", img_up.single('img'), usercontroller.createUser); 회원가입 multer 주석
 
-// router.post("/test", img_up.single('img'), async(req, res) => {
-//     // img_up.single('img'),
-//     // console.log (111111, req.file.location)
-//     const uri = req.file.location
-//     console.log(uri)
-//     // const userImage = String(uri)
-//     // const test = "1232satsdat134"
-//     // const email = "stasdtst"
-//     // const { email, nickname } = req.body;
-//     await User.create({ uri });
-//     res.status(200).json({ message: "새로운 유저 정보가 등록되었습니다" })
 
-// })
+
+
 
 
 module.exports = router;
